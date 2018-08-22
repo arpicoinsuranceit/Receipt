f7e6b2de747493cec8c96a6c49b494d917e82bc3
Anjana Thrishakya <anjanathrishakya@gmail.com>
2018-08-22 14:39:54 +0530

    v1

--- src/main/java/org/arpico/groupit/receipt/SpringBootWebApplication.java
+++ src/main/java/org/arpico/groupit/receipt/SpringBootWebApplication.java
@@ -2,11 +2,18 @@ package org.arpico.groupit.receipt;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringBootWebApplication {
+public class SpringBootWebApplication extends SpringBootServletInitializer{
 
 	public static void main (String args[]) {
 		SpringApplication.run(SpringBootWebApplication.class, args);
+	}
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(SpringBootWebApplication.class);
 	}
 }
--- src/main/java/org/arpico/groupit/receipt/util/AppConstant.java
+++ src/main/java/org/arpico/groupit/receipt/util/AppConstant.java
@@ -17,15 +17,15 @@ public class AppConstant {
 	public static final String DOC_CODE_QUOT = "QUOT";
 	public static final String SYSTEM_CREATE = "system";
 	
-	public static final String URI_GET_QUO_DETAILS = "http://localhost:8084/getQuoDetail";
-	public static final String URI_GET_QUO_SHEDULES = "http://localhost:8084/getShedule";
-	public static final String URI_GET_QUO_MEDILEIS = "http://localhost:8084/getMediDetails";
-	public static final String URI_QUOTATION_AVAILABLE = "http://localhost:8084/isavailable";
-	public static final String URI_QUOTATION_SURRENDER_VALS = "http://localhost:8084/getSurrenderVals";
-	public static final String URI_QUOTATION_PENSION_SHEDULE = "http://localhost:8084/getPensionShedule";
-	public static final String URI_QUOTATION_NOMINEE_DETAILS = "http://localhost:8084/getNominee";
-	public static final String URI_UPDATE_STATUS = "http://localhost:8084/updateStatus";
-	public static final String URI_QUOTATION_DETAILS_FROM_SEQNO = "http://localhost:8084/getQuotationDetailFromSeqNo";
+	public static final String URI_GET_QUO_DETAILS = "http://localhost:8080/Quotation/getQuoDetail";
+	public static final String URI_GET_QUO_SHEDULES = "http://localhost:8080/Quotation/getShedule";
+	public static final String URI_GET_QUO_MEDILEIS = "http://localhost:8080/Quotation/getMediDetails";
+	public static final String URI_QUOTATION_AVAILABLE = "http://localhost:8080/Quotation/isavailable";
+	public static final String URI_QUOTATION_SURRENDER_VALS = "http://localhost:8080/Quotation/getSurrenderVals";
+	public static final String URI_QUOTATION_PENSION_SHEDULE = "http://localhost:8080/Quotation/getPensionShedule";
+	public static final String URI_QUOTATION_NOMINEE_DETAILS = "http://localhost:8080/Quotation/getNominee";
+	public static final String URI_UPDATE_STATUS = "http://localhost:8080/Quotation/updateStatus";
+	public static final String URI_QUOTATION_DETAILS_FROM_SEQNO = "http://localhost:8080/Quotation/getQuotationDetailFromSeqNo";
 	
 	public static final String DOC_CODE_FOR_RECEIPT = "RCPP";
 	public static final String POLICY_STATUS_PLISU = "PLISU";
