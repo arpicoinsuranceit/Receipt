261aff4e8af5650e9ee28264da96d773781ed1b1
Harindi Supunsara <supunsara.harindi@gmail.com>
2018-09-12 14:39:14 +0530

    Receipt Cancelation Changes

---
+++ src/main/java/org/arpico/groupit/receipt/controller/ReceiptCancelationController.java
@@ -0,0 +1,44 @@
+package org.arpico.groupit.receipt.controller;
+
+import java.util.List;
+import org.arpico.groupit.receipt.service.ReceiptCancelationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin(origins = "*")
+public class ReceiptCancelationController {
+
+	@Autowired
+	private ReceiptCancelationService receiptCancelationService;
+	
+	@RequestMapping(value = "/getReceipts/{token}/{receiptId}", method = RequestMethod.GET)
+	public List<String> getAgentDtos (@PathVariable("token") String token,@PathVariable("receiptId") String receiptId){
+		
+		try {
+			return receiptCancelationService.findReceiptLikeReceiptId(receiptId, token);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return null;
+	}
+	
+	@RequestMapping(value = "/saveRequest/{token}/{receiptNo}/{reason}", method = RequestMethod.GET)
+	public ResponseEntity<Object> saveRequest(@PathVariable("receiptNo") String receiptNo,@PathVariable("reason") String reason,@PathVariable("token") String token){
+		
+		try {
+			return receiptCancelationService.saveRequest(receiptNo, reason,token);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return null;
+	}
+	
+}
