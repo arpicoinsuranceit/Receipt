4b53da7b10587b070826a902abf21d93db553ad2
DSP Chathuranga <dspchathuranga@gmail.com>
2019-02-11 10:03:45 +0530

    s

--- src/main/java/org/arpico/groupit/receipt/service/impl/PolicyReceiptServiceImpl.java
+++ src/main/java/org/arpico/groupit/receipt/service/impl/PolicyReceiptServiceImpl.java
@@ -420,17 +420,10 @@ public class PolicyReceiptServiceImpl implements PolicyReceiptService {
 //		inBillingTransactionDao.save(deposit);
 //	}
 
-<<<<<<< HEAD
-	public ReceiptPrintDto getReceiptPrintDto(InProposalsModel inProposalsModel,
-=======
-	@Transactional(readOnly = false, rollbackFor = Exception.class)
-	private void saveReceipt(InTransactionsModel inTransactionsModel, InBillingTransactionsModel deposit) {
-		inTransactionDao.save(inTransactionsModel);
-		inBillingTransactionDao.save(deposit);
-	}
+
 
 	private ReceiptPrintDto getReceiptPrintDto(InProposalsModel inProposalsModel,
->>>>>>> refs/remotes/origin/feature-agn-inq
+
 			InTransactionsModel inTransactionsModel, String agentCode, String locCode, boolean b,
 			List<HashMap<String, String>> setoffList) throws Exception {
 		ReceiptPrintDto printDto = new ReceiptPrintDto();
